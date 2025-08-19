50365572867b8838ae04ddd589b2ae003e2463fe
ywijie <yijieew@gmail.com>
2025-08-19 18:46:50 +0800

    implement echo function

--- src/main/java/Burrito.java
+++ src/main/java/Burrito.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Burrito {
     public static void main(String[] args) {
         String logo = " ____                      _        \n"
@@ -13,8 +15,29 @@ public class Burrito {
         System.out.println(lineseperator);
         System.out.println("Hello! I'm " + name + ".\nWhat can I do for you?");
         System.out.println(lineseperator);
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println(lineseperator);
+        boolean exit = true;
+        String input;
+        while (exit) {
+            Scanner scanner = new Scanner(System.in);
+            input = scanner.nextLine();
+            System.out.println(lineseperator);
+
+            if (input.equals("bye")) {
+                exit = false;
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(lineseperator);
+
+                break;
+            }
+            System.out.println(input);
+            System.out.println(lineseperator);
+
+
+
+
+
+        }
+
 
     }
 }
