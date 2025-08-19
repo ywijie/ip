e646cf11e36f4257aee40ac1110b038b8140d09d
ywijie <yijieew@gmail.com>
2025-08-19 18:11:28 +0800

    Changed name from Duke to Burrito and implemented level-0

---
+++ src/main/java/Burrito.java
@@ -0,0 +1,22 @@
+public class Burrito {
+    public static void main(String[] args) {
+        String logo = " ____                      _        \n"
+                + "|  _ \\                  (_) |       \n"
+                + "| |_) |  _   _ _ __ _ __ _| |_ ___  \n"
+                + "|  _ <  | | | | .--| .--| | __/   \\ \n"
+                + "| |_) | | |_| | |  | |  | | || (_) |\n"
+                + "|____/   \\__,_|_|  |_|  |_|\\__\\___/ \n";
+
+        String name = "Burrito";
+        String lineseperator = "____________________________________________________________";
+        System.out.println(logo);
+        System.out.println(lineseperator);
+        System.out.println("Hello! I'm " + name + ".\nWhat can I do for you?");
+        System.out.println(lineseperator);
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println(lineseperator);
+
+    }
+}
+
+
