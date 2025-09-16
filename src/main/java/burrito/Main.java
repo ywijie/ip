46b484b58bd5b7c4faa8e72083478adb58db6d27
ywijie <yijieew@gmail.com>
2025-09-16 14:55:13 +0800

    Finalize features for release
    Add title and app icon to app window.

--- src/main/java/burrito/Main.java
+++ src/main/java/burrito/Main.java
@@ -3,6 +3,7 @@ package burrito;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -18,6 +19,9 @@ public class Main extends Application {
     @Override
     public void start(Stage stage) {
         try {
+            Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
+            stage.getIcons().add(dukeImage);
+            stage.setTitle("Burrito");
             FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
